0a1f01a1f8ff8091f8535410612f16ddfb9c551b
NeryC <nalbertoc132@gmail.com>
2023-03-14 18:00:46 -0300

    test

--- utils/index.js
+++ utils/index.js
@@ -50,5 +50,5 @@ export const setNewSelectedLines = (selectedLines, item) => {
 
 export const axiosInstance = axios.create({
   // baseURL: "http://epymodel.uaa.edu.py/api",
-  baseURL: "http://localhost:30001",
+  baseURL: "http://epymodel.uaa.edu.py:30001",
 });
